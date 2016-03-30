645446ad651c9672b6628df0e35b9a529fa52766
jbolmarcich <jbolmarcich@gmail.com>
2016-03-30 15:48:22 -0400

    Rewrote to fit our metadata needs.

--- elements.php
+++ elements.php
@@ -1,52 +1,138 @@
 <?php
-// CUL LDPD profile MODS, adapted to flat representation in Omeka
-$elements = array(
-    array(
-	'name'=>'Key Date',
-	'description'=>'Key date is creation date in normalized format, or the date that can be considered the most accurate date of representation.  Use YYYY-MM-DD format.',
-	//'data_type'=> 'Date',
-	'data_type'=> 'Date Range',
-    ),
-    array(
-	'name' => 'Type of Date',
-	'description' => 'Enter the type of date represented by the key date.'
-    ), 
-    array(
-        'name' => 'Publication Place',
-	'data_type' => 'Tiny Text',
-    ),
-    array(
-        'name' => 'Publication Date',
-	'data_type' => 'Tiny Text',
-    ),
-    array(
-        'name' => 'Place of Origin',
-	'data_type' => 'Tiny Text',
-    ),
-    array(
-        'name' => 'Form/Genre', 
-    ),
-    array(
-	'name' => 'Physical Description',
-    ),
-    array(
-	'name' => 'Physical Location',
-	'data_type' => 'Tiny Text',
-    ),
-    array(
-	'name' => 'Repository Name',
-    ),
-    array(
-        'name' => 'Shelf Location',
-    ),
-    array(
-	'name' => 'Notes',
-     ),
-    array(
-	'name' => 'Language of Cataloging',
-    ),
-    array(
-	'name' => 'Digital Origin',
-	'description' => 'How the item came to be digitized.  Options are born digital, reformatted digital, digitized microfilm, or digitized other analog',
-    )
+//Plugin modified for LIS462-20, Simmons SLIS West Digital Libraries class, Spring 2016. Forked from CUL LDPD profile MODS, adapted to flat representation in Omeka
+$elements = array(    
+array(
+	'name' => 'MODS:Part_Type',
+	'description' => 'Type of part (ex. page)',
+),
+    
+array(
+	'name' => 'MODS:Part_Order',
+	'description' => 'Part order (integer)',
+),
+    
+array(
+	'name' => 'MODS:Name_Type',
+	'description' => 'Name Type (personal, corporate, conference, family)',
+),
+    
+array(
+	'name' => 'MODS:Name_NamePart_Date',
+	'description' => 'Name Dates (Birth Year--Death Year)',
+),
+    
+array(
+	'name' => 'MODS:Name_NamePart_Authority',
+	'description' => 'Name Authority',
+),
+    
+array(
+	'name' => 'MODS:Name_Role_RoleTerm',
+	'description' => 'Role of named entity; use marc relator vocabulary',
+),
+    
+array(
+	'name' => 'MODS:Name_Role_RoleTerm_Type',
+	'description' => 'RoleTerm type (text or code)',
+),
+    
+array(
+	'name' => 'MODS:Name_Role_RoleTerm_Authority',
+	'description' => 'Role Authority or Code Source',
+),
+    
+array(
+	'name' => 'MODS:Genre',
+	'description' => 'A term or terms that designate a category characterizing a particular style, form, or content, such as artistic, musical, literary composition, etc.',
+),
+    
+array(
+	'name' => 'MODS:Genre_Authority',
+	'description' => 'Use AAT',
+),
+    
+array(
+	'name' => 'MODS:OriginInfo_DateCreated_KeyDate',
+	'description' => 'Yes or No; only one date may be chosen as Key Date=Yes.',
+),
+    
+array(
+	'name' => 'MODS:OriginInfo_DateCreated_Encoding',
+	'description' => 'Date encoding',
+),
+    
+array(
+	'name' => 'MODS:OriginInfo_DateCaptured',
+	'description' => 'Date digitized',
+),
+    
+array(
+	'name' => 'MODS:OriginInfo_DateCaptured_Encoding',
+	'description' => 'Date encoding',
+),
+    
+array(
+	'name' => 'MODS:OriginInfo:Location',
+	'description' => 'Repository Address',
+),
+    
+array(
+	'name' => 'MODS:OriginInfo:RepositoryName',
+	'description' => 'Repository Name',
+),
+    
+array(
+	'name' => 'MODS:Language_LanguageTerm',
+	'description' => 'Language of the material',
+),
+    
+array(
+	'name' => 'MODS:Language_LanguageTerm_Authority',
+	'description' => 'Language term authority',
+),
+    
+array(
+	'name' => 'MODS:Language_LanguageTerm_Type',
+	'description' => 'Language term type (text or code)',
+),
+    
+array(
+	'name' => 'MODS:PhysicalDescription_DigitalOrigin',
+	'description' => 'Reformatted digital',
+),
+    
+array(
+	'name' => 'MODS:Subject_Authority',
+	'description' => 'Ex. LCSH',
+),
+    
+array(
+	'name' => 'MODS:Subject_Geographic_Authority',
+	'description' => 'Geographic subject term authority',
+),
+    
+array(
+	'name' => 'MODS:Identifier',
+	'description' => 'Put URL of item here.',
+),
+    
+array(
+	'name' => 'MODS:Identifier_Authority',
+	'description' => 'local',
+),
+    
+array(
+	'name' => 'MODS:AccessCondition_Type',
+	'description' => 'List type of access the rights statement concerns - i.e., use and reproduction',
+),
+    
+array(
+	'name' => 'MODS:RecordInfo_LanguageOfCataloging',
+	'description' => 'Metadata language',
+),
+    
+array(
+	'name' => 'MODS:RecordInfo_RecordContentSource',
+	'description' => 'Code for organization responsible for record creation',  
+      )
 );
