8f31794417bba5efb1538f705a4d79e9ae1bdb12
jbolmarcich <jbolmarcich@gmail.com>
2016-04-09 15:02:18 -0400

    Shortened prettier display trial

--- elements.php
+++ elements.php
@@ -1,558 +1,114 @@
 <?php
 //Plugin modified for LIS462-20, Simmons SLIS West Digital Libraries class, Spring 2016. Forked from CUL LDPD profile MODS, adapted to flat representation in Omeka
-$elements = array(   array(
-	'name' => 'identifier',
-      'description' => 'Use filename (without extension or numeric id)',
-),
-array(
-	'name' => 'titleInfo',
-      'description' => 'Title',
-),
-array(
-	'name' => 'part1_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part1_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part1_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part2_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part2_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part2_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part3_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part3_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part3_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part4_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part4_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part4_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part5_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part5_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part5_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part6_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part6_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part6_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part7_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part7_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part7_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part8_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part8_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part8_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part9_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part9_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part9_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part10_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part10_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part10_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part11_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part11_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part11_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part12_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part12_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part12_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part13_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part13_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part13_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part14_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part14_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part14_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part15_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part15_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part15_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part16_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part16_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part16_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part17_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part17_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part17_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part18_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part18_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part18_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part19_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part19_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part19_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part20_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part20_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part20_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part21_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part21_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part21_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part22_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part22_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part22_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part23_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part23_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part23_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part24_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part24_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part24_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part25_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part25_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part25_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part26_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part26_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part26_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part27_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part27_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part27_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'part28_id',
-      'description' => 'Use filename (part set can be repeated)',
-),
-array(
-	'name' => 'part28_type',
-      'description' => 'Type of part (ex. page)',
-),
-array(
-	'name' => 'part28_order',
-      'description' => 'Part order (integer)',
-),
-array(
-	'name' => 'name1',
-      'description' => 'Name',
-),
-array(
-	'name' => 'name1_date',
-      'description' => 'Name Dates (Birth Year--Death Year)',
-),
-array(
-	'name' => 'name1_role',
-      'description' => 'Role of named entity; use marc relator vocabulary',
-),
-array(
-	'name' => 'name2',
-      'description' => 'Name',
-),
-array(
-	'name' => 'name2_date',
-      'description' => 'Name Dates (Birth Year--Death Year)',
-),
-array(
-	'name' => 'name2_role',
-      'description' => 'Role of named entity; use marc relator vocabulary',
-),
-array(
-	'name' => 'name3',
-      'description' => 'Name',
-),
-array(
-	'name' => 'name3_date',
-      'description' => 'Name Dates (Birth Year--Death Year)',
-),
-array(
-	'name' => 'name3_role',
-      'description' => 'Role of named entity; use marc relator vocabulary',
-),
-array(
-	'name' => 'name4',
-      'description' => 'Name',
-),
-array(
-	'name' => 'name4_date',
-      'description' => 'Name Dates (Birth Year--Death Year)',
-),
-array(
-	'name' => 'name4_role',
-      'description' => 'Role of named entity; use marc relator vocabulary',
-),
-array(
-	'name' => 'name5',
-      'description' => 'Name',
-),
-array(
-	'name' => 'name5_role',
-      'description' => 'Role of named entity; use marc relator vocabulary',
-),
-array(
-	'name' => 'name6',
-      'description' => 'Name',
-),
-array(
-	'name' => 'name6_role',
-      'description' => 'Role of named entity; use marc relator vocabulary',
-),
-array(
-	'name' => 'name7',
-      'description' => 'Name',
-),
-array(
-	'name' => 'name7_role',
-      'description' => 'Role of named entity; use marc relator vocabulary',
-),
-array(
-	'name' => 'name8',
-      'description' => 'Name',
-),
-array(
-	'name' => 'name8_role',
-      'description' => 'Role of named entity; use marc relator vocabulary',
-),
-array(
-	'name' => 'name9',
-      'description' => 'Name',
-),
-array(
-	'name' => 'name9_role',
-      'description' => 'Role of named entity; use marc relator vocabulary',
-),array(
-	'name' => 'name10',
-      'description' => 'Name',
-),
-array(
-	'name' => 'name10_role',
-      'description' => 'Role of named entity; use marc relator vocabulary',
-),array(
-	'name' => 'name11',
-      'description' => 'Name',
-),
-array(
-	'name' => 'name11_role',
-      'description' => 'Role of named entity; use marc relator vocabulary',
-),array(
-	'name' => 'name12',
-      'description' => 'Name',
-),
-array(
-	'name' => 'name12_role',
-      'description' => 'Role of named entity; use marc relator vocabulary',
-)
-array(
-	'name' => 'typeofResource',
-      'description' => 'A term or terms that designate a category characterizing a particular style, form, or content, such as artistic, musical, literary composition, etc.',
-),
-array(
-	'name' => 'genre',
-      'description' => 'Use AAT',
-),
-array(
-	'name' => 'originInfo_place',
-      'description' => 'Place resource was created',
-),
-array(
-	'name' => 'originInfo_place_auth',
-      'description' => 'Use LCNAF or TGN',
-),
-array(
-	'name' => 'originInfo_datecreated',
-      'description' => 'Date orig resource created',
-),
-array(
-	'name' => 'originInfo_datecaptured',
-      'description' => 'Date digitized',
-),
-array(
-	'name' => 'language',
-      'description' => 'Language of the material',
-),
-array(
-	'name' => 'language_code',
-      'description' => 'language code',
-),
-array(
-	'name' => 'physicalDescription_extent',
-      'description' => 'extent of original resource (pages, etc)',
-),
-array(
-	'name' => 'physicalDescription_mediatype',
-      'description' => 'use internet media type list',
-),
-array(
-	'name' => 'physicalDescription_digitalorigin',
-      'description' => 'Reformatted digital',
-),
-array(
-	'name' => 'abstract',
-      'description' => 'Abstract',
-),
-array(
-	'name' => 'subject1',
-      'description' => 'Subject',
-),
-array(
-	'name' => 'subject1_auth',
-      'description' => 'Ex. LCSH',
-),
-array(
-	'name' => 'subject2',
-      'description' => 'Subject',
-),
-array(
-	'name' => 'subject2_auth',
-      'description' => 'Ex. LCSH',
-),
-array(
-	'name' => 'subject3',
-      'description' => 'Subject',
-),
-array(
-	'name' => 'subject3_auth',
-      'description' => 'Ex. LCSH',
-),
-array(
-	'name' => 'subject4',
-      'description' => 'Subject',
-),
-array(
-	'name' => 'subject4_auth',
-      'description' => 'Ex. LCSH',
-),
-array(
-	'name' => 'subject5',
-      'description' => 'Subject',
-),
-array(
-	'name' => 'subject5_auth',
-      'description' => 'Ex. LCSH',
-),
-array(
-	'name' => 'accessCondition',
-      'description' => 'List type of access the rights statement concerns - i.e., use and reproduction',
-),
-array(
-	'name' => 'recordInfo_langofcat',
-      'description' => 'Metadata language',
-),
-array(
-	'name' => 'recordInfo_contentsrc',
-      'description' => 'Code for organization responsible for record creation',
-),
+$elements = array(   
+array(
+	'name' => 'Identifier',
+	'description' => 'Identifier',
+	),
+array(	
+	'name' => 'Title',
+	'description' => 'Item title',
+	),
+array(	
+	'name' => 'Part Type',
+	'description' => 'Part type',
+	),
+array(	
+	'name' => 'Part Order',
+	'description' => 'Part order (integer)',
+	),
+array(	
+	'name' => 'Creator',
+	'description' => 'Name',
+	),
+array(	
+	'name' => 'Creator Dates',
+	'description' => 'Name Dates (Birth Year--Death Year)',
+	),
+array(	
+	'name' => 'Addressee',
+	'description' => 'Name',
+	),
+array(	
+	'name' => 'Addressee Dates',
+	'description' => 'Name Dates (Birth Year--Death Year)',
+	),
+array(	
+	'name' => 'Depicted',
+	'description' => 'Name',
+	),
+array(	
+	'name' => 'Depicted Dates',
+	'description' => 'Name Dates (Birth Year--Death Year)',
+	),
+array(	
+	'name' => 'Resource Type',
+	'description' => 'Type of resource',
+	),
+array(	
+	'name' => 'Genre',
+	'description' => 'A term or terms that designate a category characterizing a particular style, form, or content, such as artistic, musical, literary composition, etc.',
+	),
+array(	
+	'name' => 'Place Created',
+	'description' => 'Place resource was created',
+	),
+array(	
+	'name' => 'Place Created-Authority',
+	'description' => 'Geographic subject term authority',
+	),
+array(	
+	'name' => 'Date Captured',
+	'description' => 'Date digitzed',
+	),
+array(	
+	'name' => 'Language',
+	'description' => 'Language of the material',
+	),
+array(	
+	'name' => 'Language Code',
+	'description' => 'Language term type (text or code)',
+	),
+array(	
+	'name' => 'Extent',
+	'description' => 'Extent (pages, etc.)',
+	),
+array(	
+	'name' => 'Media Type',
+	'description' => 'Digital format',
+	),
+array(	
+	'name' => 'Digital Origin',
+	'description' => 'Reformatted digital',
+	),
+array(	
+	'name' => 'Abstract',
+	'description' => 'Brief description',
+	),
+array(	
+	'name' => 'Subject',
+	'description' => 'Subject',
+	),
+array(	
+	'name' => 'Subject-Authority',
+	'description' => 'Ex. LCSH',
+	),
+array(	
+	'name' => 'Related Items',
+	'description' => 'Put URL of item here.',
+	),
+array(	
+	'name' => 'Access Rights',
+	'description' => 'List type of access the rights statement concerns - i.e., use and reproduction',
+	),
+array(	
+	'name' => 'Cataloging Language',
+	'description' => 'Metadata language',
+	),
+array(	
+	'name' => 'Content Source',
+	'description' => 'Code for organization responsible for record creation',
+	),
 array(
 	'name' => 'itemRelations',
 	'description' => 'Enter the identifier for items (e.g., photographs) referenced in letters and vice versa.',
